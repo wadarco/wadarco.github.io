c222c0738e27c602aa8a1cd03ad3633e7dfa8ca0
Nyarlathotep <anhedonia@skiff.com>
2025-05-04 21:34:02 -0400

    refator: improve readability

--- app/components/content/CopyBtn.tsx
+++ app/components/content/CopyBtn.tsx
@@ -1,17 +1,14 @@
 'use client'
-import {
-  type FunctionComponent,
-  type MouseEventHandler,
-  type RefObject,
-  useRef,
-  useState,
-} from 'react'
 
-type Props = { contentElRef: RefObject<Element | null> }
+import { type MouseEventHandler, type RefObject, useRef, useState } from 'react'
 
-const CopyBtn: FunctionComponent<Props> = ({ contentElRef }) => {
-  const svgRef = useRef<HTMLDivElement>(null)
-  const [copied, setCopied] = useState(false)
+type Props = {
+  contentElRef: RefObject<Element | null>
+}
+
+export default function CopyBtn({ contentElRef }: Props) {
+  const svgRef = useRef<SVGSVGElement>(null)
+  const [isActive, setIsActive] = useState(false)
   const [isAnimating, setIsAnimating] = useState(false)
 
   const keyframes = [
@@ -33,11 +30,11 @@ const CopyBtn: FunctionComponent<Props> = ({ contentElRef }) => {
 
     navigator.clipboard.writeText(contentElRef.current.textContent)
     setIsAnimating(true)
-    await animate().then(() => setCopied(true))
+    await animate().then(() => setIsActive(true))
     await animateReverse()
 
     setTimeout(async () => {
-      await animate().then(() => setCopied(false))
+      await animate().then(() => setIsActive(false))
       await animateReverse().then(() => setIsAnimating(false))
     }, 800)
   }
@@ -48,35 +45,22 @@ const CopyBtn: FunctionComponent<Props> = ({ contentElRef }) => {
       type="button"
       onClick={setClipboard}
     >
-      <div ref={svgRef}>
-        {copied ? (
-          <svg
-            xmlns="http://www.w3.org/2000/svg"
-            width="16"
-            height="16"
-            viewBox="0 -960 960 960"
-            fill="currentColor"
-            role="img"
-            aria-label="copy-text"
-          >
-            <path d="M382-240 154-468l57-57 171 171 367-367 57 57-424 424Z" />
-          </svg>
+      <svg
+        xmlns="http://www.w3.org/2000/svg"
+        width="16"
+        height="16"
+        viewBox="0 -960 960 960"
+        fill="currentColor"
+        role="img"
+        aria-label="clipboard"
+        ref={svgRef}
+      >
+        {isActive ? (
+          <path d="M382-240 154-468l57-57 171 171 367-367 57 57-424 424Z" />
         ) : (
-          <svg
-            xmlns="http://www.w3.org/2000/svg"
-            width="16"
-            height="16"
-            viewBox="0 -960 960 960"
-            fill="currentColor"
-            role="img"
-            aria-label="copy-text"
-          >
-            <path d="M360-240q-33 0-56.5-23.5T280-320v-480q0-33 23.5-56.5T360-880h360q33 0 56.5 23.5T800-800v480q0 33-23.5 56.5T720-240H360Zm0-80h360v-480H360v480ZM200-80q-33 0-56.5-23.5T120-160v-560h80v560h440v80H200Zm160-240v-480 480Z" />
-          </svg>
+          <path d="M360-240q-33 0-56.5-23.5T280-320v-480q0-33 23.5-56.5T360-880h360q33 0 56.5 23.5T800-800v480q0 33-23.5 56.5T720-240H360Zm0-80h360v-480H360v480ZM200-80q-33 0-56.5-23.5T120-160v-560h80v560h440v80H200Zm160-240v-480 480Z" />
         )}
-      </div>
+      </svg>
     </button>
   )
 }
-
-export default CopyBtn
