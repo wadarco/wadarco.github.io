ccee81551d32d6635496d7ba77a817a1840feee2
Nyarlathotep <anhedonia@skiff.com>
2025-05-22 09:28:12 -0400

    feat(image): update test suite

--- app/lib/image/test/base64.test.ts
+++ app/lib/image/test/base64.test.ts
@@ -1,44 +1,46 @@
 import { describe, expect, test } from 'bun:test'
-import { FileSystem } from '@effect/platform'
+import { FetchHttpClient, FileSystem } from '@effect/platform'
 import { Effect } from 'effect'
 import { Image } from '../Image.ts'
 
-describe('Images', () => {
+describe('images', async () => {
   const file = new File(
     ['<svg viewBox="0 0 1 1"><path d="M0 0h1v1H0z"/></svg>'],
     'image.svg',
   )
-
   const fileSystemMock = FileSystem.layerNoop({
     readFile: () => Effect.promise(() => file.bytes()),
   })
+  const url = URL.createObjectURL(file)
+  const content = await file
+    .text()
+    .then(Buffer.from)
+    .then((_) => _.toString('base64'))
 
-  test('loaders', async () => {
-    const url = URL.createObjectURL(file)
-    const content = await file.text()
-
-    await Image.file(url)
-      .pipe(
-        Effect.map((_) => Buffer.from(_).toString('utf-8')),
-        Effect.tap((res) => expect(res).toBe(content)),
-        Effect.provide(fileSystemMock),
-        Effect.runPromise,
-      )
-      .finally(() => URL.revokeObjectURL(url))
+  test('images:metadata', async () => {
+    const metadata = await Image.make({ loader: Image.file(url) }).pipe(
+      Effect.flatMap((_) => _.metadata),
+      Effect.provide(fileSystemMock),
+      Effect.runPromise,
+    )
+    expect(metadata.format).toBe('svg')
   })
 
-  test('data:base64', async () => {
-    const url = URL.createObjectURL(file)
-    const content = await file
-      .text()
-      .then((_) => Buffer.from(_))
-      .then((_) => _.toString('base64'))
-
-    const { metadata, base64 } = await Image.make({ loader: Image.file(url) })
-      .pipe(Effect.flatMap(Effect.all), Effect.provide(fileSystemMock), Effect.runPromise)
-      .finally(() => URL.revokeObjectURL(url))
-
+  test('images:file-loader', async () => {
+    const base64 = await Image.make({ loader: Image.file(url) }).pipe(
+      Effect.flatMap((_) => _.base64),
+      Effect.provide(fileSystemMock),
+      Effect.runPromise,
+    )
     expect(base64).toBe(`data:image/svg+xml;base64,${content}`)
-    expect(metadata.format).toBe('svg')
+  })
+
+  test('images:url-loader', async () => {
+    const base64 = await Image.make({ loader: Image.url(url) }).pipe(
+      Effect.flatMap((_) => _.base64),
+      Effect.provide(FetchHttpClient.layer),
+      Effect.runPromise,
+    )
+    expect(base64).toBe(`data:image/svg+xml;base64,${content}`)
   })
 })
