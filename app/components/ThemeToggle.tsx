14e7646916f49c437ed751d6888a98236934f984
Nyarlathotep <anhedonia@skiff.com>
2025-06-19 23:21:55 -0400

    style(theme): improve readability

--- app/components/ThemeToggle.tsx
+++ app/components/ThemeToggle.tsx
@@ -1,24 +1,23 @@
 'use client'
+
 import { type LucideProps, MoonIcon, SunIcon } from 'lucide-react'
-import type { SVGProps } from 'react'
+import type { MouseEventHandler, SVGProps } from 'react'
 import { useTheme } from '~/lib/theme/useTheme.ts'
 import Button from './ui/Button.tsx'
 
-export default function ThemeToggle({
-  className,
-  ...props
-}: SVGProps<SVGSVGElement> & LucideProps) {
+type ThemeToggleProps = SVGProps<SVGSVGElement> & LucideProps
+
+export default function ThemeToggle({ className, ...props }: ThemeToggleProps) {
   const { setTheme } = useTheme()
 
+  const onClick: MouseEventHandler<HTMLButtonElement> = () => {
+    setTheme((theme) => ({
+      colorScheme: theme.colorScheme === 'dark' ? 'light' : 'dark',
+    }))
+  }
+
   return (
-    <Button
-      variant="ghost"
-      onClick={() => {
-        setTheme((theme) => ({
-          colorScheme: theme.colorScheme === 'dark' ? 'light' : 'dark',
-        }))
-      }}
-    >
+    <Button variant="ghost" onClick={onClick}>
       <SunIcon className={`inline-block dark:hidden ${className}`} {...props} />
       <MoonIcon className={`hidden dark:inline-block ${className}`} {...props} />
     </Button>
