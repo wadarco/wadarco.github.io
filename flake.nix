013934d70f86fcea4166f23bcb011b7daea47702
Nyarlathotep <anhedonia@skiff.com>
2025-06-27 02:00:58 -0400

    flake: use nix `systems.flakeExposed`

--- flake.nix
+++ flake.nix
@@ -6,33 +6,24 @@
   outputs =
     { self, nixpkgs }:
     let
-      inherit (nixpkgs.lib) genAttrs;
-      systems = [
-        "x86_64-linux"
-        "x86_64-darwin"
-        "aarch64-linux"
-        "aarch64-darwin"
-      ];
-      eachSystem = f: genAttrs systems (s: f { pkgs = nixpkgs.legacyPackages.${s}; });
+      inherit (nixpkgs.lib) genAttrs systems;
+      eachSystem = f: genAttrs systems.flakeExposed (system: f nixpkgs.legacyPackages.${system});
     in
     {
-      devShells = eachSystem (
-        { pkgs }:
-        {
-          default = pkgs.mkShell {
-            nativeBuildInputs = with pkgs; [
-              bun
-              biome
-            ];
-            env = with pkgs; {
-              BIOME_BINARY = "${biome}/bin/biome";
-              # Fix Sharp installation: depends on libstdc++.so.6
-              LD_LIBRARY_PATH = "${stdenv.cc.cc.lib}/lib:$LD_LIBRARY_PATH";
-            };
+      devShells = eachSystem (pkgs: {
+        default = pkgs.mkShell {
+          nativeBuildInputs = with pkgs; [
+            bun
+            biome
+          ];
+          env = with pkgs; {
+            BIOME_BINARY = "${biome}/bin/biome";
+            # Fix Sharp installation: depends on libstdc++.so.6
+            LD_LIBRARY_PATH = "${stdenv.cc.cc.lib}/lib:$LD_LIBRARY_PATH";
           };
+        };
 
-        }
-      );
-      formatter = eachSystem ({ pkgs }: pkgs.nixfmt-rfc-style);
+      });
+      formatter = eachSystem (pkgs: pkgs.nixfmt-rfc-style);
     };
 }
