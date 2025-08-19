66e30a6bb8c9f00db6f6716df76572a53ab02309
Nyarlathotep <anhedonia@skiff.com>
2025-08-18 21:00:45 -0400

    fix(content): correct layer initialization

--- app/lib/content/Config.ts
+++ app/lib/content/Config.ts
@@ -1,21 +1,14 @@
 import type { Options } from '@mdx-js/esbuild'
-import { Context, Effect, Layer, Ref } from 'effect'
+import { Context, Layer, Ref } from 'effect'
 
-interface ContentConfig {
+type ContentConfig = Ref.Ref<{
   readonly mdx?: Options
-}
+}>
 
 /**
  * Note: This could also be implemented using `Effect.Service`,
  * but I intentionally avoid that here to prevent introducing
  * classes into the design.
  */
-
-const Tag = Context.GenericTag<Ref.Ref<ContentConfig>>('~content/config')
-
-export const Live = Layer.effect(
-  Tag,
-  Effect.map(Ref.make<ContentConfig>({}), (config) => config),
-)
-
-export const ContentConfig = Tag.pipe(Effect.provide(Live))
+const ContentConfig = Context.GenericTag<ContentConfig>('~content/config')
+export const Live = Layer.effect(ContentConfig, Ref.make({}))
